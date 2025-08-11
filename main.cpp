8163a7ba97b57154e0c2b3dab74a6db7799fddf3
Кирилл Шевцов <shevtsov.km@phystech.edu>
2025-08-11 15:43:05 +0300

    11/08

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "chartify.h"
 #include <iostream>
+#include <cmath>
 namespace chr = Chartify;
 int main(){
     sf::String title = "My Graph";
-    auto profile = std::make_unique<chr::RenderProfile>(960, 480, title);
-    Chartify::Canvas Graph(std::move(profile), chr::Color({255, 255, 255}, 255), chr::Color({23, 45, 67}, 67), chr::Color({89, 45, 67}, 45));
+    auto profile = std::make_unique<chr::RenderProfile>();
+    std::vector<double> f = {1.5, 2.23, std::sqrt(2)}, g = {sqrt(1.5), sqrt(2.23), sqrt(sqrt(2))};
+    Chartify::Canvas Graph(std::move(profile), chr::Color({255, 255, 255}, 255), chr::Color({23, 45, 67}, 67), chr::Color({89, 45, 67}, 255), chr::AXES | chr::GRID);
+    Graph.Plot(f, g, chr::Color({0,0,255}, 255));
     Graph.Show();
 }
