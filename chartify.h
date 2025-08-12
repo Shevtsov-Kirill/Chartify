02770a977883f49002fd0ed9cfd1ef670f775cee
Кирилл Шевцов <shevtsov.km@phystech.edu>
2025-08-12 13:21:55 +0300

    12.08 13:21

--- chartify.h
+++ chartify.h
@@ -3,12 +3,13 @@
 #include <memory>
 #include <vector>
 #include <iterator>
+#include <cmath>
 namespace Chartify{
     struct Default{
         static inline unsigned int WIDTH = 1000, HEIGHT = 500;
     };
     enum Flag{
-        GRID = 1 << 0, AXES = 1 << 1
+        GRID = 1 << 0, AXES = 1 << 1, SOLID = 1 << 2, DASHED = 1 << 3, DOTTED = 1 << 4
     };
     class Color{
         uint8_t alpha_;
@@ -67,6 +68,9 @@ namespace Chartify{
         sf::RenderWindow profile_;
     public:
         RenderProfile(unsigned int width, unsigned int height, sf::String& title) : title_(title), sizes_(width, height), profile_(sf::VideoMode(sizes_.x, sizes_.y), title_){
+            if(width < 960 || height < 480){
+                throw std::invalid_argument("Small scale!");
+            }
             if(title_.isEmpty()){
                 throw std::invalid_argument("Title is absented!");
             }
@@ -96,11 +100,12 @@ namespace Chartify{
         std::vector<double> x_, y_;
         std::unique_ptr<RenderProfile> profile_;
         Color fone_, grid_, axes_, plot_;
-        unsigned int flag_;
+        unsigned int flag_, linestyle_;
     public:
         Canvas(std::unique_ptr<RenderProfile> profile, Color fone, Color grid, Color axes, unsigned int flag) : profile_(std::move(profile)), fone_(fone), grid_(grid), axes_(axes), flag_(flag){}
-        void Plot(const std::vector<double>& x, const std::vector<double>& y, Color color = Color({0, 0, 255}, 255)) {
+        void Plot(const std::vector<double>& x, const std::vector<double>& y, const Color& color, unsigned int linestyle = Flag::DOTTED) {
             plot_ = color;
+            linestyle_ = linestyle;
             x_.resize(x.size()), y_.resize(y.size());
             x_ = x;
             y_ = y;
@@ -135,7 +140,56 @@ namespace Chartify{
                 sf::Vertex yl[] = {sf::Vertex(sf::Vector2f(x, 0), axes_.Data()), sf::Vertex(sf::Vector2f(x, profile_->Data().y), axes_.Data())};
                 profile_->Profile().draw(xl, 2, sf::LineStrip), profile_->Profile().draw(yl, 2, sf::LineStrip);
             }
-            profile_->Profile().draw(plot.data(), plot.size(), sf::LineStrip);
+            switch(linestyle){
+                case Flag::SOLID:{
+                    profile_->Profile().draw(plot.data(), plot.size(), sf::LineStrip);
+                }
+                break;
+                case Flag::DASHED:{
+                    const float dash_len = 10, gap_len = 5;
+                    for(std::size_t i = 1; i < plot.size(); ++i){
+                        const sf::Vector2f& iu = plot[i - 1].position, iv = plot[i].position;
+                        sf::Vector2f div = iv - iu;
+                        float segment_len = std::sqrt(div.x * div.x + div.y * div.y);
+                        div /= segment_len;
+                        float drawn = 0;
+                        bool dash_drawn = true;
+                        while(drawn < segment_len) {
+                            float step = dash_drawn ? dash_len : gap_len;
+                            float current_step = std::min(step, segment_len - drawn);
+                            sf::Vector2f start = iu + div * drawn;
+                            sf::Vector2f end = iu + div * (drawn + current_step);
+                            
+                            if(dash_drawn) {
+                                sf::Vertex dash[] = {sf::Vertex(start, color.Data()), sf::Vertex(end, color.Data())};
+                                profile_->Profile().draw(dash, 2, sf::Lines);
+                            }
+                            
+                            drawn += current_step;
+                            dash_drawn = !dash_drawn;
+                        }
+                    }
+                }
+                break;
+                case Flag::DOTTED:{
+                    const float spacing = 10;
+                    const float R = 2.5;
+                    for(std::size_t i = 1; i < plot.size(); ++i){
+                        const sf::Vector2f& iu = plot[i - 1].position, iv = plot[i].position;
+                        sf::Vector2f div = iv - iu;
+                        float segment_len = std::sqrt(div.x * div.x + div.y * div.y);
+                        div /= segment_len;
+                        for(float s = 0.0f; s < segment_len; s += spacing){
+                            sf::Vector2f dot_position = iu + div * s;
+                            sf::CircleShape dot(R);
+                            dot.setPosition(dot_position);
+                            dot.setFillColor(color.Data());
+                            profile_->Profile().draw(dot);
+                        }
+                    }
+                }
+                break;
+            }
             profile_->Profile().display();
         }
         void Show(){
@@ -149,7 +203,7 @@ namespace Chartify{
                             break;
                     }
                 }
-                Plot(x_, y_, plot_);
+                Plot(x_, y_, plot_, linestyle_);
             }
         }
         virtual ~Canvas() = default;
